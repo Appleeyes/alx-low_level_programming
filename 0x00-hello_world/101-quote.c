8a9209e22b65de392c7335c950e1ed794b3a00e2
Appleeyes <appleeyespackage@gmail.com>
2022-12-08 11:58:44 -0800

    101-quote.c file is changed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Result: 0 (Success)
+ * Result: 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" 
+			- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
