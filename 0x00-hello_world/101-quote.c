6c9479569f8bdcf9f6a5f2090507e5df56db54ef
Appleeyes <appleeyespackage@gmail.com>
2022-12-08 12:06:52 -0800

    file updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Result: 1 (Success)
+ * Result: 1 on success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"
+		     	- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
