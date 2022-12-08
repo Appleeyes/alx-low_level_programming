8ce845e316df380c24d0b3e9ba920e014075cf11
Appleeyes <appleeyespackage@gmail.com>
2022-12-08 11:02:22 -0800

    advanced tasks added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Result: 0 (Success)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	int intType;
 	float fltType;
 	long int lgiType;
-	long long int lTp;
+	long long int lTyp;
 	char charType;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lgiType));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lTp));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lTyp));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(fltType));
 	return (0);
 }
