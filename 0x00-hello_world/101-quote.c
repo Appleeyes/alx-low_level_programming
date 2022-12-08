4fa5ee5a701a8afc73bf4f4ad8dbbe5eab6162ef
Appleeyes <appleeyespackage@gmail.com>
2022-12-08 12:52:14 -0800

    file updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,18 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - program entry point
  *
- * Result: 1 on success
+ * Return: 1 on success
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
+
+
+
+
 
