2c7b31de5833e42adb329ec84a56d9e1a39f063f
Appleeyes <appleeyespackage@gmail.com>
2022-12-09 09:42:20 -0800

    directory changed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Result: 0(Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else
+	   if (n == 0)
+		   printf("%d is zero\n", n);
+	   else
+		   print("%d is negative\n", n);
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Readme file for all 0x01-variables_if_else_while tasks
