d1d432096f166773823c11461430bec0edf3eab6
Appleeyes <appleeyespackage@gmail.com>
2022-12-09 09:53:10 -0800

    file updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,9 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-     		printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	
 	return (0);
 }
 
