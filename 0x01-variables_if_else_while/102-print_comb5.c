49129c348f33f3c695a7c9ed50f9a56fe50ab472
Appleeyes <appleeyespackage@gmail.com>
2022-12-09 16:37:00 -0800

    last file added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (p == 98 && q == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0)
+}
+
