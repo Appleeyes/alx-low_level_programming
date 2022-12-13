7a1650dcc2d9c34b01a73e9f21d9c530022145b3
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 08:44:17 -0800

    1-alphabet.c file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = "a"; i <= "z"; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char _putchar(char c)
+char _putchar(char c);
+void print_alphabet(void);
 
 #endif
 
