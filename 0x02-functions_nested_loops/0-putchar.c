42ea179b01e41d4f024bdcf9be9e525d770e4d95
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 03:25:56 -0800

    0-putchar.c file updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	char word = "_putchar";
+	char *word = "_putchar";
 
-	while (word)
+	while (*word)
 	{
-		_putchar(word);
+		_putchar(*word);
 		word++;
 	}
 	_putchar('\n');
