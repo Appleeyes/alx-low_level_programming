d211b666bb1e204db128bb84a1eb117e15255b80
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 11:55:31 -0800

    6-abs.c file added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - this is a function that computes the absolute value of an integer
+ *@b: an input charater
+ *Return: the absolute value of b
+ */
+
+int _abs(int b)
+{
+	if (b >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
+
