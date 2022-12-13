87b134111b17e2b446bb1cfe78299ff5e1870cad
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 03:18:54 -0800

    0-putchar.c and Readme file added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char word = "_putchar";
+
+	while (word)
+	{
+		_putchar(word);
+		word++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme file for 0x02-functions_nested_loops tasks
