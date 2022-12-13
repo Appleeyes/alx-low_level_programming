bbbc30a711c5f2c99fe71fee647b5f3294118220
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 11:22:32 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - this is a function that prints the sign of a number.
+ * @n: input character
+ * Return: Always 1 if n is greater than 0 || 0 if n is 0 otherwise -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
