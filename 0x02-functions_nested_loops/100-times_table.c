bdd85479e963e46b100571e96efe3ae93888e8c6
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 18:58:57 -0800

    file updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,51 +8,38 @@
 
 void print_times_table(int n)
 {
-	int a, b;
+	int a, b, c;
 
 	if (n > 0 && n < 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			_putchar('0');
+
 			for (b = 1; b <= n; b++)
-				putformat(a * b);
+			{
+				putchar(',');
+				_putchar(' ');
+
+				c = a * b;
+				if (c <= 99)
+					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10)) % 10 + '0');
+				}
+				else if (c <= 99 && prod >= 10)
+				{
+					_putchar((c / 10) + '0');
+				}
+				_putchar((c % 10) + '0');
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - formatted output characters
- * @n: input character to format
- * Return: empty
- */
-
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
 
