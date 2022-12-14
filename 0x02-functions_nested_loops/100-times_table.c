d83b4fab0086bdd43e9857de0fbb0fd7034316fb
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 18:09:41 -0800

    100-times_table.c filr updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,17 +8,6 @@
 
 void print_times_table(int n)
 {
-	int a, b;
-
-	if (n > 0 && n < 15)
-	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar('0');
-			for (b = 1; b <= n; b++)
-				_putchar(a * b);
-			_putchar('\n');
-		}
-	}
+	return (0);
 }
 
