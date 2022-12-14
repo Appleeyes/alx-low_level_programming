971f7834be13c79bcae92ffc7372f37472295952
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 20:11:10 -0800

    104-fibonacci.c file added

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long a = 0, b = 1, add;
+	unsigned long c, d, e, f;
+	unsigned long h, i;
+
+	for (count = 0; count < 92; count++)
+	{
+		add = a + b;
+		printf("%lu, ", add);
+
+		a = b;
+		b = add;
+	}
+
+	c = a / 10000000000;
+	e = b / 10000000000;
+	d = a % 10000000000;
+	f = b % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		h = c + e;
+		i = d + f;
+
+		if (d + f > 9999999999)
+		{
+			h += 1;
+			i %= 10000000000;
+		}
+		printf("%lu%lu", h, i);
+
+		if (count != 98)
+			printf(", ");
+
+		c = e;
+		d = f;
+		e = h;
+		f = i;
+	}
+	printf("\n");
+	return (0);
+}
+
