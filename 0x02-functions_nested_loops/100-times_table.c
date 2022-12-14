fb159c050068221c4638ed698e6f970e4b619678
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 18:03:29 -0800

    100-times_table.c file added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_times_table - a function that prints the n times table.
+ *@n: an input character
+ * Return: Empty
+ */
+
+void print_times_table(int n)
+{
+	int a, b;
+
+	if (n > 0 && n < 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+				_putformat(a * b);
+			_putchar('\n');
+		}
+	}
+}
+
