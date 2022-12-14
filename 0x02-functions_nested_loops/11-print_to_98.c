32b6e0922f041641e101ed85a1e523573e9fae0a
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 17:52:04 -0800

    11-print_to_98.c file updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf(", ");
 		}
 		printf("\n");
 	}
@@ -29,7 +29,7 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf(", ");
 		}
 		printf("\n");
 	}
