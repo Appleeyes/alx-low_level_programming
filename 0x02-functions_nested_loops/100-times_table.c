545db8db42871f8df2312a753d2af0ef1967125e
Appleeyes <appleeyespackage@gmail.com>
2022-12-13 18:36:16 -0800

    100-times_table.c file is updared

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,51 @@
 
 void print_times_table(int n)
 {
-	return (0);
+	int a, b;
+
+	if (n > 0 && n < 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b <= n; b++)
+				putformat(a * b);
+			_putchar('\n');
+		}
+	}
+}
+
+/**
+ * putformat - formatted output characters
+ * @n: input character to format
+ * Return: empty
+ */
+
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
 
