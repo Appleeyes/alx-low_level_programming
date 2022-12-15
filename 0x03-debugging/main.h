fdc5fb2f6ae2058f68a29834849e6cb9f35e302f
Appleeyes <appleeyespackage@gmail.com>
2022-12-15 04:40:38 -0800

    file updated

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,3 +3,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
