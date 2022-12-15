636e60e6dbb06552faa4ce249ad6784e57aba6c2
Appleeyes <appleeyespackage@gmail.com>
2022-12-15 12:02:44 -0800

    10-print_triangle.c file added

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: size of the triangle
+ * Return: Empty
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (b = 0; b <= (size - 1); b++)
+		{
+			for (b = 0; b < (size - 1) - b; a++)
+			{
+				_putchar(' ');
+			}
+			for (c = 0; c <= ro; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
