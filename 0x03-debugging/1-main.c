ead5c109c15e5c20a8a13c6de1e8ea0b09b79534
Appleeyes <appleeyespackage@gmail.com>
2022-12-15 03:06:55 -0800

    1-main.c file added

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	printf("Infinite loop incoming :(\n");
+
+	n = 0;
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
