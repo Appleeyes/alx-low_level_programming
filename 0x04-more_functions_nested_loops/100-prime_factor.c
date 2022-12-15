0c67f2a79995f7c04cf6842a12291c77e7de09bc
Appleeyes <appleeyespackage@gmail.com>
2022-12-15 12:30:50 -0800

    bug fixed

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints prime factors of 612852475143
+ * main - prints prime factors
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
 	return (0);
 }
