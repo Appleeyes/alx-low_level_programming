804d3bf159b37b16635b82766854766f05087f1e
Appleeyes <appleeyespackage@gmail.com>
2022-12-15 12:27:31 -0800

    bug fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,11 +2,22 @@
 
 /**
  * print_number - a function that prints an integer
- * @n: input
- * Return: inputted integer
+ * @n: input character
+ * Return: Empty
  */
 
 void print_number(int n)
 {
-	return (0);
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+
+	if (a > 9)
+		print_number(a / 10);
+
+	_putchar(a % 10 + '0');
 }
