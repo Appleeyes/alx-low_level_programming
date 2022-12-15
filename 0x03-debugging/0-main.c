348e81c23108585bfc72e42eb870e109f1f02e97
Appleeyes <appleeyespackage@gmail.com>
2022-12-15 02:58:34 -0800

    0-main.c file updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * positive_or_negative - a function that prints positive or negative integer
- *@i: input integer
- * Return - Empty
+ * main - a function that print postive or negative integer
+ *
+ * Return: Always 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
+	return (0);
 }
 
