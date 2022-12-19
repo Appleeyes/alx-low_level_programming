10a91cd5d231a581cd56d904fa653693901e5cf2
Appleeyes <appleeyespackage@gmail.com>
2022-12-19 10:50:36 -0800

    0-reset_to_98.c and README file added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function takes a pointer to an int as parameter
+ * and updates the value it points to 98.
+ * @n: Input character
+ * Return: Empty
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme for 0x05-pointers_arrays_strings tasks
