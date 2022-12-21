af7a91bace40c0c20a77cedb8502b591b60a6a55
Appleeyes <appleeyespackage@gmail.com>
2022-12-21 15:49:03 -0800

    new files updated

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,39 +1,24 @@
 #include "main.h"
 
 /**
-
-  * reverse_array - reverses the content of an array of integers
-
-  * @n: size
-
-  * @a: input character
-
-  * Return: Empty
-
+ * reverse_array - reverses the content of an array of integers
+ * @n: size
+ * @a: input character
+ * Return: Empty
  **/
 
 void reverse_array(int *a, int n)
-
 {
 
 	int aux = 0, i, j;
 
 	for (i = 0; i < n; i++)
-
 	{
-
 	for (j = 0; j < (n - i - 1); j++)
-
 	{
-
 	aux = a[j + 1];
-
 	a[j + 1] = a[j];
-
 	a[j] = aux;
-
 	}
-
 	}
-
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,37 +1,24 @@
 #include "main.h"
 
 /**
-
-  * string_toupper - changes all lowercase letters of a string to uppercase
-
-  * @n: size
-
-  * Return: Empty
-
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase
+ * @n: size
+ * Return: Empty
  **/
 
 char *string_toupper(char *n)
-
 {
 
 	int i = 0;
 
 	while (n[i] != '\0')
-
 	{
-
 	if (n[i] < 123 && n[i] > 96)
-
 	{
-
 	n[i] = (n[i] - 32);
-
 	}
-
 	i++;
-
 	}
-
 return (n);
-
 }
