00a31941e531f5ed9512125bd46a2da771d671cd
Apple <appleeyespackage@gmail.com>
2022-12-22 00:59:36 +0100

    Update 103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+  * infinite_add - adds two numbers
+  * @n1: input character
+  * @n2: another input character
+  * @r: result
+  * @size_r: size result
+  * Return: result of the addition
+**/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, a, b, c, n, aux, dec = 0;
+
+	while (n1[i] != '\0')
+	{
+	i++;
+	}
+	while (n2[j] != '\0')
+	{
+	j++;
+	}
+	for (n = 0; n < j || n < i; n++)
+	{
+	a = (i - n) > 0 ? (n1[i - n - 1] - '0') : 0;
+	b = (j - n) > 0 ? (n2[j - n - 1] - '0') : 0;
+	c = a + b + dec;
+	r[n] = (c % 10) + '0';
+	dec = c > 9 ? 1 : 0;
+	}
+	if (dec == 1)
+	{ r[n] = '1';
+	r[n + 1] = '\0'; }
+	else
+	{ r[n] = '\0';
+	n--; }
+	for (i = 0; i < n + 1; i++)
+	{
+	for (j = 0; j < (n - i); j++)
+	{
+	aux = r[j + 1];
+	r[j + 1] = r[j];
+	r[j] = aux;
+	}
+	}
+return (n < size_r - 1 ? r : 0);
+}
