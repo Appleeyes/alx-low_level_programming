9f895af0b5e2fc1f331cb1343cb8af386730b354
Apple <appleeyespackage@gmail.com>
2022-12-22 00:56:08 +0100

    Update 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_number - prints number with _putchar
+  * @n: input character
+  * Return: Empty
+ **/
+void print_number(int n)
+{
+	unsigned int number;
+
+	number = n;
+	if (n < 0)
+	{
+	_putchar('-');
+	number = -n;
+	}
+	if (number / 10 != 0)
+	{
+	print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
+}
