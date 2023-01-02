5396ab9584b37cc74b053639d5ec024ee7df4b6b
Apple <appleeyespackage@gmail.com>
2023-01-02 23:50:21 +0100

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: input character
+ * @to: input character
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
