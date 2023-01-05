7f8bd1d13af7b2b18436b190cfe71c29f4ea5f71
Apple <appleeyespackage@gmail.com>
2023-01-05 23:24:21 +0100

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - Entry Point
+ * @s1: input character
+ * @s2: another input character
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
