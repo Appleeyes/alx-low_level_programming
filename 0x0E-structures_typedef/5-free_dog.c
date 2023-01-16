69ac151ccca6744291419fe364150b07e0900b99
Apple <appleeyespackage@gmail.com>
2023-01-16 19:10:34 +0000

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * free_dog - Entry Point
- * @d: struct to free
- * Return: 0
+ * free_dog - frees a dog structure
+ * @d: pointer to struct to free
+ * Return: Nothing
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
 	free(d);
 }
