4166f558a64b73561b06f0432dc49df6563d1e66
Apple <appleeyespackage@gmail.com>
2023-01-31 02:33:54 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,30 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_list - A function that prints a linked list
- * @h: pointer t  head of list
- * Return: number of nodes in list as size_t
+ * print_list- entry level
+ * @h: the list being passed
+ * Return: size of the passed in list
  */
+
 size_t print_list(const list_t *h)
 {
-	unsigned int nodes = 0;
+	size_t counter = 0;
 
 	while (h)
 	{
-		printf("[%u] ", h->len);
+		counter++;
+
 		if (h->str == NULL)
-			printf("(nil)\n");
-		else
-			printf("%s\n", h->str);
+		{
+			printf("[0] (nil)\n");
+		} else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		h = h->next;
-		nodes++;
 	}
-	return (nodes);
+
+	return (counter);
 }
