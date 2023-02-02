27f13a787bda7f76ef5bdea0a4c3318018845195
Apple <appleeyespackage@gmail.com>
2023-02-03 00:13:18 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,28 +7,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, count, k, temp;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	temp = n;
-
-	while (temp != 0)
-	{
-		i++;
-		temp = temp >> 1;
-	}
-
-	for (count = i - 1; count >= 0; count--)
-	{
-		k = n >> count;
-		if (k & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+	_putchar((n & 1) + '0');
 }
