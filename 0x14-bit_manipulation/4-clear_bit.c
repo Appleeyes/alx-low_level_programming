65192c388d38d5b63ef640a3c590601beeb55edc
Apple <appleeyespackage@gmail.com>
2023-02-03 00:27:01 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,15 +7,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bits;
-
-	if (n == NULL)
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
 
-	bits = 1 << index;
-
-	if ((bits | *n) == *n)
-		*n = *n ^ bits;
+	*n &= ~(1 << index);
 
 	return (1);
 }
