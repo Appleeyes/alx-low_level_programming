00acd7855e1b4f04487fe9cd842d8680e0759a45
Apple <appleeyespackage@gmail.com>
2023-02-03 00:22:31 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,14 +8,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bits;
-
-	if (n == NULL)
+	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
 
-	bits = 1 << index;
-
-	*n = *n | bits;
+	*n |= (1 << index);
 
 	return (1);
 }
