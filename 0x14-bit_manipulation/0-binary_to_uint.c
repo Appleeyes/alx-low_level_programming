a8a8bf3833d403044bf30f781972c6ace62d2d31
Apple <appleeyespackage@gmail.com>
2023-02-02 23:40:01 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - A finction that converts binary number
+ * binary_to_uint - function that converts binary number
  * to an unsigned integer
  * @b: A pointer to binary string
  * Return: unsigned integer converted number
