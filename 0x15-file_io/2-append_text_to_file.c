a4ad82620df2c4b79427702b344541f77932f025
Apple <appleeyespackage@gmail.com>
2023-02-10 23:28:02 +0000

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,62 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
- * append_text_to_file - Entry Point
- * @filename: file name
- * @text_content: text content
- * Return: 1
+ * append_text_to_file - apends text to file
+ * @filename: path to file
+ * @text_content: content
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, i = 0;
+	int fd;
+	ssize_t w;
+	int size;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (file == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
 		return (-1);
 
-
-
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
+	if (!text_content)
 	{
-		close(file);
+		close(fd);
 		return (1);
 	}
-	else
+
+	size = _strlen(text_content);
+	w = write(fd, text_content, size);
+
+	if (w == -1)
 	{
-		wr = write(file, text_content, i);
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - len
+ *
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
 	}
 
-	if (wr == -1)
-		return (-1);
-
-	close(file);
-	return (1);
+	return (i);
 }
