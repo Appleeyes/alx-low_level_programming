5d68dabbd9554748d481332ad6a17376604c1bb3
Apple <appleeyespackage@gmail.com>
2023-02-10 23:31:03 +0000

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
+#ifndef IO_
+#define IO_
 int _putchar(char c);
+int _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 #endif
