c801ceac034bb96ce775dc8703161aecbab1bc47
Apple <appleeyespackage@gmail.com>
2023-02-10 23:12:58 +0000

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,40 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
 /**
- * append_text_to_file - A function that appends text at the end to the  file
- * @filename: The filename to open and append in
- * @text_content: The NULL terminated string to add
- * Return: 1 on success, -1 if the file can not be created, nor written,
- * nor write fails.
+ * append_text_to_file - Entry Point
+ * @filename: file name
+ * @text_content: text content
+ * Return: 1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fdo, fdw, len = 0;
+	int file, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fdo = open(filename, O_RDWR | O_APPEND);
-	if (fdo < 0)
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
 		return (-1);
+
+
+
+	while (text_content[i])
+		i++;
+
 	if (text_content == NULL)
 	{
-		close(fdo);
+		close(file);
 		return (1);
 	}
+	else
+	{
+		wr = write(file, text_content, i);
+	}
 
-	while (*(text_content + len))
-		len++;
-
-	fdw = write(fdo, text_content, len);
-	close(fdo);
-	if (fdw < 0)
+	if (wr == -1)
 		return (-1);
 
+	close(file);
 	return (1);
 }
