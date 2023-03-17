5e9a28fb3d1d9facc0797d45a2845ddf303dc526
Apple <appleeyespackage@gmail.com>
2023-03-17 04:36:29 +0000

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -10,6 +10,8 @@
  * @prev: points to the previous node
  * @next: points to the next node
  *
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
