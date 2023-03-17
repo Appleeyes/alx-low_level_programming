c7ead91dc945c1a99caf82d4a73c9314aadb355c
Apple <appleeyespackage@gmail.com>
2023-03-17 04:24:50 +0000

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,38 +1,29 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning
- * of a dlistint_t list
+ * dlistint_len - returns the number of elements in
+ * a double linked list
  *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
+ * @h: head of the list
+ * Return: the number of nodes
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t *new;
-	dlistint_t *h;
+	int count;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	count = 0;
 
-	new->n = n;
-	new->prev = NULL;
-	h = *head;
+	if (h == NULL)
+		return (count);
 
-	if (h != NULL)
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		while (h->prev != NULL)
-			h = h->prev;
+		count++;
+		h = h->next;
 	}
 
-	new->next = h;
-
-	if (h != NULL)
-		h->prev = new;
-
-	*head = new;
-
-	return (new);
+	return (count);
 }
